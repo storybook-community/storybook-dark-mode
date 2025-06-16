658ef3c739d47cc632e68c345ffed565affb7653
unional <homawong@gmail.com>
2025-06-15 18:10:17 -0700

    chore: disable screenshotFailures

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
 			enabled: true,
 			headless: true,
 			provider: 'playwright',
-			instances: [{ browser: 'chromium' }]
+			instances: [{ browser: 'chromium' }],
+			screenshotFailures: false
 		},
 		setupFiles: ['.storybook/vitest.setup.ts']
 	}
