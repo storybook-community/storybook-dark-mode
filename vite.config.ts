e7fe7e404144a3070fab2a3019b1085836224834
unional <homawong@gmail.com>
2025-06-15 18:09:40 -0700

    chore: use vite defineConfig in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [tailwindcss()],
