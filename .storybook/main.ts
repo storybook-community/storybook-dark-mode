56288a1ede2a1e2668cb1364daaf3b3e0a9dc814
Jeppe Reinhold <jeppe@reinhold.is>
2025-07-02 22:08:12 +0200

    rename preset to preset.cjs to match CJS content

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 export default {
 	stories: ['../src/**/*.stories.tsx', '../src/**/*.mdx'],
-	addons: ['@storybook/addon-docs', '@storybook/addon-vitest', '../preset.js'],
+	addons: ['@storybook/addon-docs', '@storybook/addon-vitest', '../preset.cjs'],
 	framework: {
 		name: '@storybook/react-vite',
 		options: {}
