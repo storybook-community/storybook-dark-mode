6c6ceb4ec20fd7ad58c5d8f6f6c41c4ab30d45b3
k-utsumi <kaba.box@gmail.com>
2025-10-07 11:45:13 +0900

    🐛 Add createRequire for ESM compatibility in preset
    Signed-off-by: k-utsumi <kaba.box@gmail.com>

--- preset.js
+++ preset.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+
 export function managerEntries(entry = []) {
 	return [...entry, require.resolve('./esm/manager/index.js')]
 }
