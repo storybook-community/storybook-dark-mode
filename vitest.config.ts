0a77c21e29705d23ebc33a078bbfa976dda2b23b
unional <homawong@gmail.com>
2025-12-02 01:06:25 -0800

    chore: add @repobuddy/vitest dependency and configure Vitest
    Signed-off-by: unional <homawong@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
+import { browserTestPreset } from '@repobuddy/vitest/config'
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { playwright } from '@vitest/browser-playwright'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	plugins: [tailwindcss(), storybookTest()],
+	plugins: [
+		tailwindcss(),
+		storybookTest(),
+		browserTestPreset({
+			includeGeneralTests: true
+		})
+	],
 	optimizeDeps: {
 		include: ['react/jsx-dev-runtime']
 	},
