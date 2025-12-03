3f7cda81cb75bca562da77fb8400bf977eae041c
unional <homawong@gmail.com>
2025-12-03 00:30:41 -0800

    feat: Remove `DarkModeStore` type from main export
    Signed-off-by: unional <homawong@gmail.com>

---
+++ .changeset/quiet-facts-fry.md
@@ -0,0 +1,6 @@
+---
+"@storybook-community/storybook-dark-mode": minor
+---
+
+Remove `DarkModeStore` type from main export.
+It should be internal to begin with.
--- packages/storybook-dark-mode/src/internal/utils/store.ts
+++ packages/storybook-dark-mode/src/internal/utils/store.ts
@@ -1,6 +1,28 @@
 import { dequal as equal } from 'dequal'
+import type { ThemeVars } from 'storybook/theming'
 import { themes } from 'storybook/theming'
-import type { DarkModeStore } from '../../types'
+import type { Mode } from '../../types'
+
+export interface DarkModeStore {
+	/** The class target in the preview iframe */
+	classTarget: string
+	/** The current mode the storybook is set to */
+	current: Mode
+	/** The dark theme for storybook */
+	dark: ThemeVars
+	/** The dark class name for the preview iframe */
+	darkClass: string | string[]
+	/** The light theme for storybook */
+	light: ThemeVars
+	/** The light class name for the preview iframe */
+	lightClass: string | string[]
+	/** Apply mode to iframe */
+	stylePreview: boolean
+	/** Persist if the user has set the theme */
+	userHasExplicitlySetTheTheme: boolean
+	/** Disable the dark mode toggle */
+	disable: boolean
+}
 
 const { document, window } = globalThis
 
